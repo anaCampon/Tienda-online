4e5123beb67bfaf082dbf0b9a2107500312ca71a
anaCampon <anatrcp@gmail.com>
2025-02-02 15:48:56 +0100

    Proyecto tienda online V1

---
+++ data.js
@@ -0,0 +1,98 @@
+let productos = [
+    {
+        id: 1,
+        nombre: "Cuerda Beal Karma",
+        imagen: "cuerda-beal-karma.JPG",
+        descripcion: "Cuerda de Escalada Beal Karma 9.8 mm",
+        precio: 120,
+        stock: 15
+    },
+    {
+        id: 2,
+        nombre: "Arnés Petzl Corax",
+        imagen: "arnes-petzl-corax.JPG",
+        descripcion: "Arnés ajustable y cómodo, diseñado para escalada deportiva.",
+        precio: 65,
+        stock: 10
+    },
+    {
+        id: 3,
+        nombre: "Mosquetón Black Diamond Rocklock",
+        imagen: "mosqueton-black-diamond.JPG",
+        descripcion: "Con cierre de seguridad, perfecto para aseguramiento y rápel.",
+        precio: 18,
+        stock: 50
+    },
+    {
+        id: 4,
+        nombre: "Pies de Gato La Sportiva Miura",
+        imagen: "pies-de-gato-miura.JPG",
+        descripcion: "Calzado de escalada de alto rendimiento.",
+        precio: 130,
+        stock: 20
+    },
+    {
+        id: 5,
+        nombre: "Casco Mammut Wall Rider",
+        imagen: "casco-mammut-wall-rider.JPG",
+        descripcion: "Casco ultraligero y resistente, diseñado para escaladores exigentes.",
+        precio: 85,
+        stock: 8
+    },
+    {
+        id: 6,
+        nombre: "Bolsa para Magnesio Ocún",
+        imagen: "bolsa-magnesio-ocun.JPG",
+        descripcion: "Bolsa para magnesio con diseño ergonómico, incluye cinturón ajustable.",
+        precio: 20,
+        stock: 35
+    },
+    {
+        id: 7,
+        nombre: "Juego de Friends Wild Country",
+        imagen: "friends-wild-country.JPG",
+        descripcion: "Set de 5 friends para protección en fisuras, fiables y fáciles de colocar.",
+        precio: 250,
+        stock: 12
+    },
+    {
+        id: 8,
+        nombre: "Crashpad Black Diamond Mondo",
+        imagen: "crashpad-mondo.JPG",
+        descripcion: "Colchoneta de alta densidad, ofrece excelente protección en caídas.",
+        precio: 300,
+        stock: 5
+    },
+    {
+        id: 9,
+        nombre: "Guantes Petzl Cordex",
+        imagen: "guantes-petzl-cordex.JPG",
+        descripcion: "Guantes ligeros y resistentes para aseguramiento y rápel.",
+        precio: 35,
+        stock: 25
+    },
+    {
+        id: 10,
+        nombre: "Juego de Cintas Express Edelrid",
+        imagen: "cintas-edelrid.JPG",
+        descripcion: "Set de 6 cintas express ligeras y resistentes para escalada deportiva.",
+        precio: 90,
+        stock: 18
+    },
+    {
+        id: 11,
+        nombre: "Cuerda Estática Tendon 10.5 mm",
+        imagen: "static-tendon.JPG",
+        descripcion: "Cuerda estática para uso en trabajos verticales y rescate.",
+        precio: 89,
+        stock: 25
+    },
+    {
+        id: 12,
+        nombre: "Arnés Black Diamond Momentum",
+        imagen: "Black-Diamond-Momentum.JPG",
+        descripcion: "Arnés versátil y cómodo para escalada deportiva y tradicional.",
+        precio: 54,
+        stock: 30
+    },
+]
---
+++ main.js
@@ -0,0 +1,187 @@
+// Añado evento para ocultar menú hamburguesa en versión móvil
+function showMenu() {
+    let burgerMenu = document.querySelector('.desplegable .fa-solid');
+    let navegador = document.querySelector('nav');
+    burgerMenu.addEventListener('click', function(){
+        navegador.classList.toggle('hidden')
+    })
+
+    //Para solucionar que si lo oculto y amplio la pantalla se vea siempre el menú
+        window.addEventListener('resize', function () {
+            if (window.innerWidth >= 768) {
+                navegador.classList.remove('hidden');
+            }
+        });
+}
+
+
+let productCount = [0]//Para utilizar un contador para cada ID incluidos en carrito
+
+
+//Se inicia así la página con los productos que hay en data.js
+function showPills(){
+    let container = document.querySelector('.fondo');
+    for (let producto of productos){
+        let $pill = document.createElement('div');
+        $pill.innerHTML = `
+            <img src = 'Images/${producto.imagen}' >
+            <h1> ${producto.nombre} </h1>
+            <p> ${producto.descripcion} </p>
+            <p> Precio: ${producto.precio} €</p>
+            <button id = 'addCarrito'> Agregar al Carrito </button>
+        `
+        $pill.id = producto.id
+        $pill.classList.add('pill');
+        container.appendChild($pill);
+        //Agrego funcionalidad al botón #addCarrito aprovechando el bucle
+        let addToCartBtn = $pill.querySelector('#addCarrito')
+        addToCartBtn.addEventListener('click', addToCart);
+    }
+}
+
+//Para que aparezca y desaparezca el div del carrito
+function showCart() {
+    let cart = document.querySelector('.cart');
+    cart.classList.toggle('hidden')
+}
+
+
+function addToCart(addToCartBtn){
+    let cart = document.querySelector('.cart')
+    let pill = this.parentElement
+    let productContainer = document.querySelector('.productContainer')
+    // Abrir ventana de carrito
+    if (cart.classList.contains('hidden')){
+        showCart();
+    }
+
+    
+        //Si el producto ya está en el carrito, aumentar la cantidad
+        //Compruebo ya que si no está vacía esa posición del array, da true
+        if(productCount[pill.id] > 0){
+            addOneMore(pill) 
+        } else {
+            //Si no está en el carrito, lo agrego.
+            productCount[pill.id] = 1
+            //Añado el id a que ya existe el array
+            let $product = document.createElement('div');
+            $product.innerHTML = `
+                    <p> ${productos[pill.id-1].nombre} </p>
+                    <p class='multiply'>${productos[pill.id-1].precio} € X ${productCount[pill.id]} </p>
+                    <div class= 'buttonContainer'>
+                    <button id = 'delete'> Eliminar </button>
+                    <button id = 'add'> + </button>
+                    <button id = 'subtract'> - </button>
+                    </div>
+                `
+            $product.classList.add('producto')
+            //Añado el ID de data.js por eso es pill.id-1, ya que la posición del array no coincide con el ID
+            $product.id = productos[pill.id-1].id
+            productContainer.appendChild($product);
+            //Añado un addEventListener a cada botón que hay 
+            let deleteBtns = document.querySelectorAll('#delete');
+            for (let deleteBtn of deleteBtns){
+                deleteBtn.addEventListener('click', deleteProduct);
+            }
+
+            let addBtns = document.querySelectorAll('#add');
+            for (let addBtn of addBtns){
+                addBtn.addEventListener('click', addOne);
+            }
+
+            let subtractBtns = document.querySelectorAll('#subtract');
+            for (let subtractBtn of subtractBtns){
+                subtractBtn.addEventListener('click', subtractOne);
+            }
+
+        }  
+        showTotal()  
+}
+
+function addOne(){
+    //Función para añadir desde el botón de +
+    let div = this.parentElement.parentElement
+    if(productCount[div.id] >= productos[div.id-1].stock){
+        alert('no hay stock')
+    } else {
+        productCount[div.id]++
+        let p = div.querySelector('.multiply')
+        p.textContent = `${productos[div.id-1].precio} € X ${productCount[div.id]}`
+    }
+    showTotal()
+}
+
+function addOneMore(pill){
+    //Si no hay stock, mando un alert
+    if (productCount[pill.id] >= productos[pill.id-1].stock){
+        alert('no hay stock')
+    } else {
+        //Hago un array para tener un contador con la posición de cada ID
+        productCount[pill.id]++
+        //el precio lo cojo de data.js y el contador del array que he creado
+        let price = (productos[pill.id-1].precio)*productCount[pill.id]
+        let div = document.getElementById(pill.id)
+        let p = div.querySelector('.multiply')
+        //Edito el texto de dentro del ID del carrito
+        p.textContent = `${productos[pill.id-1].precio} € X ${productCount[pill.id]}`
+    }
+    showTotal()
+}
+
+function subtractOne() {
+    let div = this.parentElement.parentElement
+    if (productCount[div.id] > 1){
+        productCount[div.id]--
+        let p = div.querySelector('.multiply')
+        p.textContent = `${productos[div.id-1].precio} € X ${productCount[div.id]}`
+    }
+    showTotal()
+}
+
+function deleteProduct(){
+    console.log()
+    let div = this.parentElement.parentElement;
+    productCount.splice(div.id, 1, 0);
+    div.remove();
+    showTotal();
+}
+
+function showTotal(){
+    let sumaTotal = 0
+    let total = document.querySelector('#total')
+    let $productos = document.querySelectorAll('.producto')
+    for (let $producto of $productos) {
+        sumaTotal += (productos[$producto.id-1].precio) * productCount[$producto.id] 
+    }
+    total.textContent = `total: ${sumaTotal} €`
+}
+
+function emptyCart(){
+    let $productos = document.querySelectorAll('.producto')
+    for (let $producto of $productos) {
+        $producto.remove();
+    }
+    productCount = [0]
+    showTotal();
+}
+
+function buy(){
+    let $productos = document.querySelectorAll('.producto')
+    if ($productos.length > 0){
+        alert('Compra realizada con éxito. ¡Gracias por su compra!')
+    } else {
+        alert ('El carrito está vacío')
+    }
+}
+
+let cart = document.querySelector('#cart-shopping');
+cart.addEventListener('click', showCart);
+
+let emptyCartBtn = document.querySelector('#emptyCart');
+emptyCartBtn.addEventListener('click', emptyCart);
+
+let buyBtn = document.querySelector('#buy');
+buyBtn.addEventListener('click', buy);
+
+showMenu()
+showPills()
